c22088e7ef52d0a81e5e70cc194e7e5c46f089f3
k0ner <konradtendera@gmail.com>
2016-07-30 10:34:04 +0200

    add vimrc.local.plugins and vimrc.local.before

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 cp ./zshrc ~/.zshrc
 
+cp ./vimrc.local ~/.vimrc.local
+cp ./vimrc.local.plugins ~/.vimrc.local.plugins
+cp ./vimrc.local.before ~/.vimrc.local.before
 curl vimfiles.luan.sh/install | bash
 
-cp ./vimrc.local ~/.vimrc.local
 cp ./tmux.conf ~/.tmux.conf
 

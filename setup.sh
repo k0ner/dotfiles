ba640424f5953111deb54ff7b483765bac95b92a
k0ner <konradtendera@gmail.com>
2017-07-14 19:12:17 -0500

    update tmux.conf

--- setup.sh
+++ setup.sh
@@ -1,14 +1,4 @@
-brew install fish
-curl -L https://get.oh-my.fish | fish
-omf install simple-ass-prompt
-omf theme simple-ass-prompt
-
-mkdir -p ~/.config/fish/
-cp ./config.fish ~/.config/fish/
-
 cp ./vimrc.local ~/.vimrc.local
 cp ./vimrc.local.plugins ~/.vimrc.local.plugins
 cp ./vimrc.local.before ~/.vimrc.local.before
 curl vimfiles.luan.sh/install | bash
-
-cp ./tmux.conf ~/.tmux.conf
